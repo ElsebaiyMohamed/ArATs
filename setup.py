4a73bc6908bf21ca13523c8d294c969331b3b288
elsebaiy elsebaiy192156 <elsebaiy192156@fci.bu.edu.eg>
2023-04-07 02:40:31 +0200

    S

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name = "aang",
-    version = "0.0.8",
+    version = "0.0.9",
     author = "seba3y",
     author_email = "sebaeymohamed43@gmail.com",
     description = "speech translation",
