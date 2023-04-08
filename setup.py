3b5ed3f3e1d9d7aa069c2852a6c5811eb9f6b7b0
Elsebaiy Mohamed <77920008+ElsebaiyMohamed@users.noreply.github.com>
2023-04-08 15:04:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name = "aang",
-    version = "0.1.4",
+    version = "0.1.5",
     author = "seba3y",
     author_email = "sebaeymohamed43@gmail.com",
     description = "speech translation",
